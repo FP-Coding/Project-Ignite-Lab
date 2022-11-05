548b342ae03668d4593146e21fe296198540a3e7
Felipe Pinto <felipe14062000@gmail.com>
2022-11-05 12:46:50 -0300

    organização estrutura de pastas

---
+++ storybook-static/assets/preview.d863d0c1.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.2ef8fc4c.js";import"./iframe.d3f4c9fe.js";import"./es.map.constructor.fa907920.js";import"./es.number.to-fixed.18b34050.js";import"./index.6f38c04c.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.d863d0c1.js.map
