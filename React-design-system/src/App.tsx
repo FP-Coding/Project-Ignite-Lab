d908e5f54ac55bac5f57b32f2dd04cba761a7ae3
Felipe Pinto <felipe14062000@gmail.com>
2022-11-05 12:43:31 -0300

    Deploy Storybook

--- React-design-system/src/App.tsx
+++ React-design-system/src/App.tsx
@@ -3,7 +3,7 @@ import './styles/global.css'
 function App() {
   return (
     <div>
-      <h1 className='bg-cyan-500 text-2xl'>Hello Word!</h1>
+      <h1>Hello Word!</h1>
     </div>
   )
 }
--- React-design-system/src/components/TextInput.stories.tsx
+++ React-design-system/src/components/TextInput.stories.tsx
@@ -1,17 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Envelope } from 'phosphor-react'
+import { Envelope, Lock } from 'phosphor-react'
 import { TextInput, TextInputInputProps , TextInputRootProps } from './TextInput'
 
 export default {
   title: 'Components/TextInput',
   component: TextInput.Root,
   args: {
-    children: [
-        <TextInput.Icon>
-          <Envelope />
-        </TextInput.Icon>,
-        <TextInput.Input placeholder='Type your e-mail address'/>
-    ],
+    children: (
+      <TextInput.Input placeholder='Type your e-mail address'/>
+    )
   },
   argTypes: {
     children: {
@@ -24,9 +21,25 @@ export default {
 
 export const Default: StoryObj<TextInputRootProps> = {};
 
-export const WhitouIcon: StoryObj<TextInputRootProps> = {
+export const withEmailIcon: StoryObj<TextInputRootProps> = {
   args: {
-    children: (<TextInput.Input placeholder='Type your e-mail address'/>)
+    children: [
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder='Type your e-mail address'/>
+    ],
+  }
+};
+
+export const withLockIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: [
+      <TextInput.Icon>
+        <Lock />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder='Type your e-mail address'/>
+    ],
   }
 };
 
